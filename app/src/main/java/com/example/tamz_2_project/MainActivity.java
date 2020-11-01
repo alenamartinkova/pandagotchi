a7ab615c4b77d83253a38434628df4a6b04c7fb5
Alena Martinkova <martinkova.a@gmail.com>
2020-11-01 11:58:09 +0100

    init

---
+++ app/src/main/java/com/example/tamz_2_project/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tamz_2_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
